c294e6e711506e0ad06dcc6da5cd19a873a1baea
sxxujin <sujn1025@naver.com>
2026-09-09 09:49:31 +0900

    S03 basic setting

---
+++ Assets/S03/polygon.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class S03_CustomPolygonMesh_Square : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+}
