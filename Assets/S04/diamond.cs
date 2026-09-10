6094e53fcebeeb9e29c7d8e01c1b04a772347854
sxxujin <sujn1025@naver.com>
2026-09-10 10:13:34 +0900

    top tetrahedron

--- Assets/S04/diamond.cs
+++ Assets/S04/diamond.cs
@@ -17,6 +17,12 @@ public class S04_CustomCubeMesh : MonoBehaviour
 
         int[] triangles = new int[]
         {
+            3, 0, 1,
+            3, 1, 2,
+            4, 3, 0,
+            4, 0, 1,
+            4, 1, 2,
+            4, 2, 3,
         };
 
         Mesh mesh = new Mesh();
