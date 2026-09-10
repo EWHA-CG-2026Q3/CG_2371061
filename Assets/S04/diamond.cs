d513238ec2160509f0aad495c10c0b00de35f1de
sxxujin <sujn1025@naver.com>
2026-09-10 10:11:32 +0900

    bottom square

---
+++ Assets/S04/diamond.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class S04_CustomCubeMesh : MonoBehaviour
+{
+    void Start()
+    {
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(-1f, 0f, -1f), // 0
+            new Vector3(1f, 0f, -1f), // 1
+            new Vector3(1f, 0f, 1f), // 2
+            new Vector3(-1f, 0f, 1f), // 3
+            new Vector3(0f, 1f, 0f), // 4
+            new Vector3(0f, -1f, 0f), // 5
+        };
+
+        int[] triangles = new int[]
+        {
+        };
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+        GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+    }
+}
